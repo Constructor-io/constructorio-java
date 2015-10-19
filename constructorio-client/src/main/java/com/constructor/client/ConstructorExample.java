a68167ad43a0a72f98b911b69d25326d7195aab8
Howon Lee <howon@howonlee.com>
2015-10-19 12:17:14 -0700

    example fleshed out for queries only

--- constructorio-client/src/main/java/com/constructor/client/ConstructorExample.java
+++ constructorio-client/src/main/java/com/constructor/client/ConstructorExample.java
@@ -1,16 +1,37 @@
-package com.constructor.client
+package com.constructor.client;
+
+import com.mashape.unirest.http.*;
 
 class ConstructorExample {
-	public main(String[] args) {
-		switch (args[0]) {
-			case "query":
-				// assertions?
+	public void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Usage: constructor (query | add | remove | modify | trackSearch | trackConversion | trackClickThrough) args...");
+			// exit
+		}
+		String firstArg = args[0];
+		// big if else in order to avoid dependency on java 7
+		if (firstArg.equals("query")) {
+			try {
 				ConstructorIO constructor = new ConstructorIO("-", "P03bVBcmyYjSG1ZQyD4V", true, null);
 				for (int i = 1; i < args.length; i++) {
-					JsonNode res = constructor.query(s);
+					JsonNode res = constructor.query(args[i]);
 					System.out.println(res.toString());
 				}
-			default:
-				System.out.println("Usage: constructor (query | add | remove | modify | trackSearch | trackConversion | trackClickThrough) args...");
+			} catch (ConstructorException con) {
+				System.out.println("fuck fuck fuck");
+			}
+		} else if (firstArg.equals("add")) {
+			//
+		} else if (firstArg.equals("remove")) {
+			//
+		} else if (firstArg.equals("modify")) {
+			//
+		} else if (firstArg.equals("trackSearch")) {
+			//
+		} else if (firstArg.equals("trackConversion")) {
+			//
+		} else if (firstArg.equals("trackClickThrough")) {
+			//
 		}
 	}
+}

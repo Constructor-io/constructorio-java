a4e225fa00a827791f1ac9c4bc975511b23da673
Howon Lee <howon@howonlee.com>
2015-10-19 17:14:16 -0700

    lots of dog food

--- constructorio-client/test.sh
+++ constructorio-client/test.sh
@@ -1,1 +1,2 @@
-mvn exec:java -Dexec.mainClass="com.constructor.client.ConstructorExample" -Dexec.args="query bo"
+#mvn exec:java -Dexec.mainClass="com.constructor.client.ConstructorExample" -Dexec.args="query bo"
+mvn exec:java -Dexec.mainClass="com.constructor.client.ConstructorExample" -Dexec.args='add YSOxV00F0Kk2R0KnPQN8 ZqXaOfXuBWD4s3XzCI1q Stanley_Steamer "Search Suggestions"'

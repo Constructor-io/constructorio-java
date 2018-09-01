f1f36c6ba9409c010f654336a7cb24da4427d1bc
Zubin Tiku <rubikzube@gmail.com>
2018-08-31 20:25:48 -0700

    Updated the constructor item

--- constructorio-client/src/main/java/io/constructor/client/ConstructorItem.java
+++ constructorio-client/src/main/java/io/constructor/client/ConstructorItem.java
@@ -7,17 +7,17 @@ import com.google.gson.Gson;
 
 public class ConstructorItem {
 
-    public String itemName;
-    public String autocompleteSection;
-    public Integer suggestedScore;
-    public ArrayList<String> keywords;
-    public String url;
-    public String imageUrl;
-    public String id;
-    public String description;
-    public HashMap <String, String> facets;
-    public HashMap <String, String> metadata;
-    public ArrayList<String> groupIds;
+    private String itemName;
+    private String autocompleteSection;
+    private int suggestedScore;
+    private ArrayList<String> keywords;
+    private String url;
+    private String imageUrl;
+    private String id;
+    private String description;
+    private HashMap <String, String> facets;
+    private HashMap <String, String> metadata;
+    private ArrayList<String> groupIds;
 
     /**
      * Creates an autocomplete item.  Optional public fields are in the <a href="https://constructor.io/docs/#add-an-item">API documentation</a>
@@ -74,5 +74,159 @@ public class ConstructorItem {
         params.put("group_ids", this.groupIds);
         Gson gson = new Gson();
         return gson.toJson(params);
+    }
+
+    /**
+     * @return the itemName
+     */
+    public String getItemName() {
+        return itemName;
+    }
+ 
+    /**
+     * @param itemName the itemName to set
+     */
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    /**
+     * @return the autocompleteSection
+     */
+    public String getAutocompleteSection() {
+        return autocompleteSection;
+    }
+
+    /**
+     * @param autocompleteSection the autocompleteSection to set
+     */
+    public void setAutocompleteSection(String autocompleteSection) {
+        this.autocompleteSection = autocompleteSection;
+    }
+    
+    /**
+     * @return the suggestedScore
+     */
+    public int getSuggestedScore() {
+        return suggestedScore;
+    }
+
+    /**
+     * @param suggestedScore the suggestedScore to set
+     */
+    public void setSuggestedScore(int suggestedScore) {
+        this.suggestedScore = suggestedScore;
+    }
+
+    /**
+     * @return the keywords
+     */
+    public ArrayList<String> getKeywords() {
+        return keywords;
+    }
+
+    /**
+     * @param keywords the keywords to set
+     */
+    public void setKeywords(ArrayList<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the imageUrl
+     */
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    /**
+     * @param imageUrl the imageUrl to set
+     */
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the facets
+     */
+    public HashMap<String, String> getFacets() {
+        return facets;
+    }
+    
+    /**
+     * @param facets the facets to set
+     */
+    public void setFacets(HashMap<String, String> facets) {
+        this.facets = facets;
+    }
+
+    /**
+     * @return the metadata
+     */
+    public HashMap<String, String> getMetadata() {
+        return metadata;
+    }
+    
+    /**
+     * @param metadata the metadata to set
+     */
+    public void setMetadata(HashMap<String, String> metadata) {
+        this.metadata = metadata;
+    }
+
+    /**
+     * @return the groupIds
+     */
+    public ArrayList<String> getGroupIds() {
+        return groupIds;
+    }
+
+    /**
+     * @param groupIds the groupIds to set
+     */
+    public void setGroupIds(ArrayList<String> groupIds) {
+        this.groupIds = groupIds;
     }
 }
--- constructorio-client/src/test/java/io/constructor/client/ConstructorItem_New_Test.java
+++ constructorio-client/src/test/java/io/constructor/client/ConstructorItem_New_Test.java
@@ -1,11 +1,9 @@
 package io.constructor.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import java.util.*;
 
-import org.apache.commons.lang.ObjectUtils.Null;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -36,9 +34,54 @@ public class ConstructorItem_New_Test {
         String itemName = this.getRandString();
         String autocompleteSection = "Suggested Searches";
         ConstructorItem item = new ConstructorItem(itemName, autocompleteSection);
-        assertNotNull(item);
-        assertEquals(item.autocompleteSection, autocompleteSection);
-        assertEquals(item.itemName, itemName);
+        assertEquals(item.getItemName(), itemName);
+        assertEquals(item.getAutocompleteSection(), autocompleteSection);
     }
 
+    @Test
+    public void newShouldReturnDefaultProperties() throws Exception {
+        String itemName = this.getRandString();
+        String autocompleteSection = "Suggested Searches";
+        ConstructorItem item = new ConstructorItem(itemName, autocompleteSection);
+        assertEquals(item.getItemName(), itemName);
+        assertEquals(item.getAutocompleteSection(), autocompleteSection);
+        assertEquals(item.getSuggestedScore(), 0);
+        assertEquals(item.getKeywords().size(), 0);
+        assertEquals(item.getUrl(), "");
+        assertEquals(item.getImageUrl(), "");
+        assertEquals(item.getDescription(), "");
+        assertEquals(item.getId(), "");
+        assertEquals(item.getFacets().size(), 0);
+        assertEquals(item.getFacets().size(), 0);
+        assertEquals(item.getGroupIds().size(), 0);
+    }
+
+    @Test
+    public void settersShouldSet() throws Exception {
+        String itemName = this.getRandString();
+        String autocompleteSection = "Suggested Searches";
+        ConstructorItem item = new ConstructorItem(itemName, autocompleteSection);
+
+        item.setItemName("airline tickets");
+        item.setAutocompleteSection("sectional airlines");
+        item.setSuggestedScore(100000);
+        item.setKeywords(new ArrayList<>(Arrays.asList("London", "Tokyo", "New ")));
+        item.setUrl("https://constructor.io/test");
+        item.setImageUrl("https://constructor.io/test.png");
+        item.setDescription("See the world!");
+        item.setId("lucky ticket 7");
+        item.setGroupIds(new ArrayList<>(Arrays.asList("Lucky Tickets", "Special Tickets", "Fancy Tickets")));
+
+        assertEquals(item.getItemName(), "airline tickets");
+        assertEquals(item.getAutocompleteSection(), "sectional airlines");
+        assertEquals(item.getSuggestedScore(), 100000);
+        assertEquals(item.getKeywords().size(), 3);
+        assertEquals(item.getUrl(), "https://constructor.io/test");
+        assertEquals(item.getImageUrl(), "https://constructor.io/test.png");
+        assertEquals(item.getDescription(), "See the world!");
+        assertEquals(item.getId(), "lucky ticket 7");
+        assertEquals(item.getFacets().size(), 0);
+        assertEquals(item.getMetadata().size(), 0);
+        assertEquals(item.getGroupIds().size(), 3);
+    }
 } 

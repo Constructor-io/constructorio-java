f609c46584183357375510f13e3228e7459be711
Jimmy Li <jimmy@constructor.io>
2018-09-18 14:46:54 -0700

    Update UserInfo class and add UserInfoTest

--- constructorio-client/src/main/java/io/constructor/client/UserInfo.java
+++ constructorio-client/src/main/java/io/constructor/client/UserInfo.java
@@ -15,8 +15,8 @@ public class UserInfo {
    * @param clientId the user's Client ID
    */
   public UserInfo(int sessionId, String clientId) {
-    this.sessionId = sessionId;
-    this.clientId = clientId;
+    this.setSessionId(sessionId);
+    this.setClientId(clientId);
   }
 
   /**
@@ -31,5 +31,31 @@ public class UserInfo {
    */
   public int getSessionId() {
     return this.sessionId;
+  }
+
+  /**
+   * Validates and sets the session id
+   *
+   * @param sessionId the user's session id
+   */
+  private void setSessionId(int sessionId) throws IllegalArgumentException {
+    if (sessionId <= 0) {
+      throw new IllegalArgumentException("Session ID cannot be less than or equal to 0.");
+    } else {
+      this.sessionId = sessionId;
+    }
+  }
+
+  /**
+   * Validates and sets the client id
+   *
+   * @param clientId the user's client id
+   */
+  private void setClientId(String clientId) throws IllegalArgumentException {
+    if (clientId == null || clientId.trim().isEmpty()) {
+      throw new IllegalArgumentException("Client ID cannot be null or an empty string.");
+    } else {
+      this.clientId = clientId;
+    }
   }
 }

353aecd59c30174fd64aa7068b2d099b0d0a332a
Jimmy Li <jimmy@constructor.io>
2018-10-29 13:38:29 -0700

    Add route, model, and tests for GET Item

---
+++ constructorio-client/src/main/java/io/constructor/client/models/Item.java
@@ -0,0 +1,112 @@
+package io.constructor.client.models;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Constructor Item ... uses Gson/Reflection to load data in
+ */
+public class Item {
+
+  @SerializedName("id")
+  private String id;
+  
+  @SerializedName("name")
+  private String name;
+  
+  @SerializedName("description")
+  private String description;
+  
+  @SerializedName("url")
+  private String url;
+  
+  @SerializedName("image_url")
+  private String imageUrl;
+
+  @SerializedName("suggested_score")
+  private Integer suggestedScore;
+
+  @SerializedName("keywords")
+  private List<String> keywords;
+
+  @SerializedName("group_ids")
+  private List<String> groupIds;
+
+  @SerializedName("facets")
+  private Map<String, Object> facets;
+
+  @SerializedName("metadata")
+  private Map<String, String> metadata;
+
+  /**
+   * @return the id
+   */
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * @return the description
+   */
+  public String getDescription() {
+    return description;
+  }
+  
+  /**
+   * @return the product url
+   */
+  public String getUrl() {
+    return url;
+  }
+
+  /**
+   * @return the image url
+   */
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  /**
+   * @return the suggested score
+   */
+  public Integer getSuggestedScore() {
+    return suggestedScore;
+  }
+  
+  /**
+   * @return the keywords
+   */
+  public List<String> getKeywords() {
+    return keywords;
+  }
+
+  /**
+   * @return the group ids
+   */
+  public List<String> getGroupIds() {
+    return groupIds;
+  }
+
+  /**
+   * @return the facets
+   */
+  public Map<String, Object> getFacets() {
+    return facets;
+  }
+
+  /**
+   * @return the metadata
+   */
+  public Map<String, String> getMetadata() {
+    return metadata;
+  }
+}
--- constructorio-client/src/test/java/io/constructor/client/ConstructorIOItemsTest.java
+++ constructorio-client/src/test/java/io/constructor/client/ConstructorIOItemsTest.java
@@ -1,10 +1,17 @@
 package io.constructor.client;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import io.constructor.client.models.Item;
 
 public class ConstructorIOItemsTest {
 
@@ -79,5 +86,34 @@ public class ConstructorIOItemsTest {
         Utils.createProductItem()
       };
       assertTrue("batch removal succeeds", constructor.removeItemBatch(items, "Products"));
+    }
+
+    @Test
+    public void getItemAsJSONShouldReturnJSON() throws Exception {
+        ConstructorIO constructor = new ConstructorIO("YSOxV00F0Kk2R0KnPQN8", "ZqXaOfXuBWD4s3XzCI1q", true, null);
+        String actualJSON = "{\"description\": \"Keep yourself dry and cozy on rainy days with this stylish rain coat that can complement any outfit.\", \"facets\": {\"Brand\": \"ROTCURTSNOC\", \"Colors\": [\"red\", \"black\", \"purple\"]}, \"id\": \"893092\", \"image_url\": \"https://constructor.io/images/893092.jpg\", \"keywords\": [\"jacket\", \"coat\", \"rain\"], \"metadata\": {\"price\": \"$50.00\"}, \"name\": \"ROTRUCTSNOC Rainy Day Coat\", \"suggested_score\": 52000, \"url\": \"https://constructor.io/pdp/893092\"}";
+        String json = constructor.getItemAsJSON("893092", "Products");
+
+        assertEquals("get items as json returns json", actualJSON, json);
+    }
+
+    @Test
+    public void getItemShouldReturnItem() throws Exception {
+        ConstructorIO constructor = new ConstructorIO("YSOxV00F0Kk2R0KnPQN8", "ZqXaOfXuBWD4s3XzCI1q", true, null);
+        Item item = constructor.getItem("893092", "Products");
+        Map<String, Object> actualFacets = new HashMap<String, Object>();
+        actualFacets.put("Brand", "ROTCURTSNOC");
+        actualFacets.put("Colors", Arrays.asList("red", "black", "purple"));
+        Map<String, String> actualMetadata = new HashMap<String, String>();
+        actualMetadata.put("price", "$50.00");
+
+        assertEquals(item.getName(), "ROTRUCTSNOC Rainy Day Coat");
+        assertEquals(item.getId(), "893092");
+        assertEquals(item.getDescription(), "Keep yourself dry and cozy on rainy days with this stylish rain coat that can complement any outfit.");
+        assertEquals(item.getUrl(), "https://constructor.io/pdp/893092");
+        assertEquals(item.getImageUrl(), "https://constructor.io/images/893092.jpg");
+        assertEquals(item.getKeywords(), Arrays.asList("jacket", "coat", "rain"));
+        assertEquals(item.getFacets(), actualFacets);
+        assertEquals(item.getMetadata(), actualMetadata);
     }
 }

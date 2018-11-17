98b9a70cae01ae816da593f03bd99d603c02a671
Zubin Tiku <rubikzube@gmail.com>
2018-11-16 16:56:30 -0800

    Added user segments to user info

--- constructorio-client/src/test/java/io/constructor/client/ConstructorIOBasicTest.java
+++ constructorio-client/src/test/java/io/constructor/client/ConstructorIOBasicTest.java
@@ -2,6 +2,8 @@ package io.constructor.client;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -54,23 +56,8 @@ public class ConstructorIOBasicTest {
         assertEquals("apiKey is set", url.queryParameter("key"), "doinkaKey");
     }
 
-
     @Test
-    public void makeUrlShouldReturnAUrlWithUserInfo() throws Exception {
-        ConstructorIO constructor = new ConstructorIO("boinkaToken", "doinkaKey", true, null);
-        UserInfo info = new UserInfo(2, "sideshow bob", "bob-id-123");
-        HttpUrl url = constructor.makeUrl("getitUuuurl", info);
-        assertEquals("host is set", url.host(), "ac.cnstrc.com");
-        assertEquals("protocol is set", url.scheme(), "https");
-        assertEquals("version is set", url.queryParameter("c"), "ciojava-4.8.0");
-        assertEquals("apiKey is set", url.queryParameter("key"), "doinkaKey");
-        assertEquals("session id is set", url.queryParameter("s"), "2");
-        assertEquals("client id is set", url.queryParameter("i"), "sideshow bob");
-        assertEquals("user id is set", url.queryParameter("ui"), "bob-id-123");
-    }
-
-    @Test
-    public void makeUrlWithoutUserIdShouldReturnAUrlWithUserInfo() throws Exception {
+    public void makeUrlWithUserIdShouldReturnAUrl() throws Exception {
         ConstructorIO constructor = new ConstructorIO("boinkaToken", "doinkaKey", true, null);
         UserInfo info = new UserInfo(2, "sideshow bob");
         HttpUrl url = constructor.makeUrl("getitUuuurl", info);
@@ -81,6 +68,38 @@ public class ConstructorIOBasicTest {
         assertEquals("session id is set", url.queryParameter("s"), "2");
         assertEquals("client id is set", url.queryParameter("i"), "sideshow bob");
         assertEquals("user id is not set", url.queryParameter("ui"), null);
+    }
+
+    @Test
+    public void makeUrlWithUserInfoShouldReturnAUrl() throws Exception {
+        ConstructorIO constructor = new ConstructorIO("boinkaToken", "doinkaKey", true, null);
+        UserInfo info = new UserInfo(2, "sideshow bob");
+        info.setUserId("bob-id-123");
+        HttpUrl url = constructor.makeUrl("getitUuuurl", info);
+        assertEquals("host is set", url.host(), "ac.cnstrc.com");
+        assertEquals("protocol is set", url.scheme(), "https");
+        assertEquals("version is set", url.queryParameter("c"), "ciojava-4.8.0");
+        assertEquals("apiKey is set", url.queryParameter("key"), "doinkaKey");
+        assertEquals("session id is set", url.queryParameter("s"), "2");
+        assertEquals("client id is set", url.queryParameter("i"), "sideshow bob");
+        assertEquals("user id is set", url.queryParameter("ui"), "bob-id-123");
+    }
+
+   
+    @Test
+    public void makeUrlWithUserSegmentsShouldReturnAUrl() throws Exception {
+        ConstructorIO constructor = new ConstructorIO("boinkaToken", "doinkaKey", true, null);
+        UserInfo info = new UserInfo(2, "sideshow bob");
+        info.setUserSegments(Arrays.asList("Jocks", "Theater kids"));
+        HttpUrl url = constructor.makeUrl("getitUuuurl", info);
+        assertEquals("host is set", url.host(), "ac.cnstrc.com");
+        assertEquals("protocol is set", url.scheme(), "https");
+        assertEquals("version is set", url.queryParameter("c"), "ciojava-4.8.0");
+        assertEquals("apiKey is set", url.queryParameter("key"), "doinkaKey");
+        assertEquals("session id is set", url.queryParameter("s"), "2");
+        assertEquals("client id is set", url.queryParameter("i"), "sideshow bob");
+        assertEquals("user segments are set", url.queryParameterValues("us").get(0), "Jocks");
+        assertEquals("user segments are set", url.queryParameterValues("us").get(1), "Theater kids");
     }
 
     @Test

01e393623dd80e8fa7c1211a14321a029345dba5
Zubin Tiku <rubikzube@gmail.com>
2020-06-17 22:08:54 -0400

    Updated code

--- constructorio-client/src/test/java/io/constructor/client/ConstructorIOBrowseTest.java
+++ constructorio-client/src/test/java/io/constructor/client/ConstructorIOBrowseTest.java
@@ -16,22 +16,22 @@ public class ConstructorIOBrowseTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    /*@Test
+    @Test
     public void createBrowseResponseShouldReturnAResult() throws Exception {
-        String string = Utils.getTestResource("response.browse.peanut.json");
+        String string = Utils.getTestResource("response.browse.color.blue.json");
         BrowseResponse response = ConstructorIO.createBrowseResponse(string);
-        assertEquals("browse facets exist", response.getResponse().getFacets().size(), 4);
+        assertEquals("browse facets exist", response.getResponse().getFacets().size(), 7);
         assertEquals("browse facet [Brand] exists", response.getResponse().getFacets().get(0).getName(), "Brand");
-        assertEquals("browse facet [Claims] exists", response.getResponse().getFacets().get(1).getName(), "Claims");
-        assertEquals("browse facet [Price] exists", response.getResponse().getFacets().get(2).getName(), "Price");
-        assertEquals("browse facet [Rating] exists", response.getResponse().getFacets().get(3).getName(), "Rating");
-        assertEquals("browse groups exist", response.getResponse().getGroups().size(), 3);
-        assertEquals("browse group [Dairy] exists", response.getResponse().getGroups().get(0).getDisplayName(), "Dairy");
-        assertEquals("browse group [Dairy] children exist", response.getResponse().getGroups().get(0).getChildren().size(), 3);
-        assertEquals("browse results exist", response.getResponse().getResults().size(), 24);
+        assertEquals("browse facet [Color] exists", response.getResponse().getFacets().get(1).getName(), "Color");
+        assertEquals("browse facet [Gender] exists", response.getResponse().getFacets().get(2).getName(), "Gender");
+        assertEquals("browse facet [Made In Italy] exists", response.getResponse().getFacets().get(3).getName(), "Made In Italy");
+        assertEquals("browse groups exist", response.getResponse().getGroups().size(), 1);
+        assertEquals("browse group [All] exists", response.getResponse().getGroups().get(0).getDisplayName(), "All");
+        assertEquals("browse group [All] children exist", response.getResponse().getGroups().get(0).getChildren().size(), 5);
+        assertEquals("browse results exist", response.getResponse().getResults().size(), 5);
         assertEquals("total number of results", (int)response.getResponse().getTotalNumberOfResults(), 562);
         assertTrue("browse result id exists", response.getResultId() != null);
-    }*/
+    }
 
     @Test
     public void BrowseShouldReturnAResult() throws Exception {
@@ -61,22 +61,10 @@ public class ConstructorIOBrowseTest {
         ConstructorIO constructor = new ConstructorIO("", "key_dKjn8oS8czBw7Ebv", true, null);
         UserInfo userInfo = new UserInfo(3, "c62a-2a09-faie");
         BrowseRequest request = new BrowseRequest("Color", "Blue");
-        request.setPage(4);
+        request.setPage(19);
         BrowseResponse response = constructor.browse(request, userInfo);
-        assertEquals("browse results exist", response.getResponse().getResults().size(), 14);
+        assertEquals("browse results exist", response.getResponse().getResults().size(), 22);
         assertEquals("browse results count as expected", (int)response.getResponse().getTotalNumberOfResults(), 562);
-        assertTrue("browse result id exists", response.getResultId() != null);
-    }
-
-    @Test
-    public void BrowseShouldReturnAResultWithBrowseSuggestionsSection() throws Exception {
-        ConstructorIO constructor = new ConstructorIO("", "key_dKjn8oS8czBw7Ebv", true, null);
-        UserInfo userInfo = new UserInfo(3, "c62a-2a09-faie");
-        BrowseRequest request = new BrowseRequest("Color", "Blue");
-        request.setSection("Browse Suggestions");
-        BrowseResponse response = constructor.browse(request, userInfo);
-        assertEquals("browse results exist", response.getResponse().getResults().size(), 17);
-        assertEquals("browse results count as expected", (int)response.getResponse().getTotalNumberOfResults(), 17);
         assertTrue("browse result id exists", response.getResultId() != null);
     }
 
@@ -85,10 +73,10 @@ public class ConstructorIOBrowseTest {
         ConstructorIO constructor = new ConstructorIO("", "key_dKjn8oS8czBw7Ebv", true, null);
         UserInfo userInfo = new UserInfo(3, "c62a-2a09-faie");
         BrowseRequest request = new BrowseRequest("Color", "Blue");
-        request.setGroupId("431");
+        request.setGroupId("sale");
         BrowseResponse response = constructor.browse(request, userInfo);
         assertEquals("browse results exist", response.getResponse().getResults().size(), 30);
-        assertEquals("browse results count as expected", (int)response.getResponse().getTotalNumberOfResults(), 95);
+        assertEquals("browse results count as expected", (int)response.getResponse().getTotalNumberOfResults(), 321);
         assertTrue("browse result id exists", response.getResultId() != null);
     }
 
@@ -97,10 +85,10 @@ public class ConstructorIOBrowseTest {
         ConstructorIO constructor = new ConstructorIO("", "key_dKjn8oS8czBw7Ebv", true, null);
         UserInfo userInfo = new UserInfo(3, "c62a-2a09-faie");
         BrowseRequest request = new BrowseRequest("Color", "Blue");
-        request.getFacets().put("Brand", Arrays.asList("Back to Nature", "Barbara's"));
+        request.getFacets().put("Brand", Arrays.asList("Frankie", "Cycle"));
         BrowseResponse response = constructor.browse(request, userInfo);
-        assertEquals("browse results exist", response.getResponse().getResults().size(), 2);
-        assertEquals("browse results count as expected", (int)response.getResponse().getTotalNumberOfResults(), 2);
+        assertEquals("browse results exist", response.getResponse().getResults().size(), 17);
+        assertEquals("browse results count as expected", (int)response.getResponse().getTotalNumberOfResults(), 17);
         assertTrue("browse result id exists", response.getResultId() != null);
     }
 

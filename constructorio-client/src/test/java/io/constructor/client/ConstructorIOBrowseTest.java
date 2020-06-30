00b69907455d7646b02421462b321d946f93ef5e
Jimmy Li <jimmyli921@gmail.com>
2020-06-29 20:06:17 -0700

    Update and fix tests

--- constructorio-client/src/test/java/io/constructor/client/ConstructorIOBrowseTest.java
+++ constructorio-client/src/test/java/io/constructor/client/ConstructorIOBrowseTest.java
@@ -57,14 +57,14 @@ public class ConstructorIOBrowseTest {
 
     @Test
     public void createBrowseResponseShouldReturnAResultWithSortOptions() throws Exception {
-        String string = Utils.getTestResource("response.search.shirt.json");
+        String string = Utils.getTestResource("response.browse.color.blue.json");
         BrowseResponse response = ConstructorIO.createBrowseResponse(string);
-        assertEquals("browse result [sort options] exists", response.getResponse().getSortOptions().size(), 1);
+        assertEquals("browse result [sort options] exists", response.getResponse().getSortOptions().size(), 4);
         assertEquals("browse result sort option [display name] exists", response.getResponse().getSortOptions().get(0).getDisplayName(), "Relevance");
         assertEquals("browse result sort option [sort by] exists", response.getResponse().getSortOptions().get(0).getSortBy(), "relevance");
         assertEquals("browse result sort option [sort order] exists", response.getResponse().getSortOptions().get(0).getSortOrder(), "descending");
         assertEquals("browse result sort option [status] exists", response.getResponse().getSortOptions().get(0).getStatus(), "selected");
-        assertEquals("total number of results", (int) response.getResponse().getTotalNumberOfResults(), 261);
+        assertEquals("total number of results", (int) response.getResponse().getTotalNumberOfResults(), 562);
         assertTrue("browse result id exists", response.getResultId() != null);
     }
 
@@ -128,23 +128,23 @@ public class ConstructorIOBrowseTest {
     }
 
     @Test
-    public void SearchShouldReturnAResultWithVariations() throws Exception {
+    public void BrowseShouldReturnAResultWithVariations() throws Exception {
         ConstructorIO constructor = new ConstructorIO("", "key_dKjn8oS8czBw7Ebv", true, null);
         UserInfo userInfo = new UserInfo(3, "c62a-2a09-faie");
         BrowseRequest request = new BrowseRequest("Color", "Blue");
         BrowseResponse response = constructor.browse(request, userInfo);
         assertEquals("browse results exist", response.getResponse().getResults().size(), 30);
-        assertEquals("browse result [variations] exists", response.getResponse().getResults().get(0).getVariations().size(), 8);
+        assertEquals("browse result [variations] exists", response.getResponse().getResults().get(0).getVariations().size(), 25);
         assertEquals("browse result variation [facets] exists", response.getResponse().getResults().get(0).getVariations().get(0).getData().getFacets().size(), 8);
-        assertEquals("browse result variation [value] exists", response.getResponse().getResults().get(0).getVariations().get(0).getValue(), "Coat Aspesi blue");
-        assertEquals("browse result variation [variation id] exists", response.getResponse().getResults().get(0).getVariations().get(0).getData().getVariationId(), "M0E20000000ECTT");
-        assertEquals("browse result variation [url] exists", response.getResponse().getResults().get(0).getVariations().get(0).getData().getUrl(), "https://demo.commercetools.com/en/aspesi-coat-I502997385098-blue.html");
+        assertEquals("browse result variation [value] exists", response.getResponse().getResults().get(0).getVariations().get(0).getValue(), "Sneakers ”H222” Hogan light blue");
+        assertEquals("browse result variation [variation id] exists", response.getResponse().getResults().get(0).getVariations().get(0).getData().getVariationId(), "M0E20000000DVZF");
+        assertEquals("browse result variation [url] exists", response.getResponse().getResults().get(0).getVariations().get(0).getData().getUrl(), "https://demo.commercetools.com/en/hogan-sneakers-38714173-lightblue.html");
         assertEquals("browse results count as expected", (int) response.getResponse().getTotalNumberOfResults(), 562);
         assertTrue("browse result id exists", response.getResultId() != null);
     }
 
     @Test
-    public void SearchShouldReturnAResultWithSortOptions() throws Exception {
+    public void BrowseShouldReturnAResultWithSortOptions() throws Exception {
         ConstructorIO constructor = new ConstructorIO("", "key_dKjn8oS8czBw7Ebv", true, null);
         UserInfo userInfo = new UserInfo(3, "c62a-2a09-faie");
         BrowseRequest request = new BrowseRequest("Color", "Blue");

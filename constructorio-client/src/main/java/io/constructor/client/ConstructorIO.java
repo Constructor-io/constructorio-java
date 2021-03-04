a1c6caac79b5ba20175720db72b4e6a575935b45
Zubin Tiku <rubikzube@gmail.com>
2021-03-04 01:00:41 -0500

    Version Bump

--- constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
+++ constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
@@ -944,7 +944,7 @@ public class ConstructorIO {
      * @return version number
      */
     protected String getVersion() {
-      return "ciojava-5.6.0";
+      return "ciojava-5.7.0";
     }
 
     /**

31344b821124887dc5628fe73b3d308d74fbf8e9
Zubin Tiku <rubikzube@gmail.com>
2021-03-04 00:57:41 -0500

    Fixes

--- constructorio-client/src/test/java/io/constructor/client/ConstructorIOBrowseTest.java
+++ constructorio-client/src/test/java/io/constructor/client/ConstructorIOBrowseTest.java
@@ -148,7 +148,7 @@ public class ConstructorIOBrowseTest {
     }
 
     @Test
-    public void SearchShouldReturnAResultWithMaxGroupsDepthOf3() throws Exception {
+    public void BrowseShouldReturnAResultWithMaxGroupsDepthOf3() throws Exception {
         ConstructorIO constructor = new ConstructorIO("", "key_dKjn8oS8czBw7Ebv", true, null);
         UserInfo userInfo = new UserInfo(3, "c62a-2a09-faie");
         BrowseRequest request = new BrowseRequest("Color", "Blue");
@@ -158,15 +158,15 @@ public class ConstructorIOBrowseTest {
         FilterGroup firstGen = root.getChildren().get(0);
         FilterGroup secondGen = firstGen.getChildren().get(0);
         FilterGroup thirdGen = secondGen.getChildren().get(0);
-        assertEquals("search result [root] exists", root.getGroupId(), "all");
-        assertEquals("search result [firstGen] exists", firstGen.getGroupId(), "sale");
-        assertEquals("search result [secondGen] exists", secondGen.getGroupId(), "sale|men");
-        assertEquals("search result [thirdGen] exists", thirdGen.getGroupId(), "sale|men|clothing");
-        assertTrue("search result id exists", response.getResultId() != null);
+        assertEquals("browse result [root] exists", root.getGroupId(), "all");
+        assertEquals("browse result [firstGen] exists", firstGen.getGroupId(), "sale");
+        assertEquals("browse result [secondGen] exists", secondGen.getGroupId(), "sale|men");
+        assertEquals("browse result [thirdGen] exists", thirdGen.getGroupId(), "sale|men|clothing");
+        assertTrue("browse result id exists", response.getResultId() != null);
     }
 
     @Test
-    public void SearchShouldReturnAResultWithMaxGroupsDepthOf1() throws Exception {
+    public void BrowseShouldReturnAResultWithMaxGroupsDepthOf1() throws Exception {
         ConstructorIO constructor = new ConstructorIO("", "key_dKjn8oS8czBw7Ebv", true, null);
         UserInfo userInfo = new UserInfo(3, "c62a-2a09-faie");
         BrowseRequest request = new BrowseRequest("Color", "Blue");
@@ -174,9 +174,9 @@ public class ConstructorIOBrowseTest {
         BrowseResponse response = constructor.browse(request, userInfo);
         FilterGroup root = response.getResponse().getGroups().get(0);
         FilterGroup firstGen = root.getChildren().get(0);
-        assertEquals("search result [root] exists", root.getGroupId(), "all");
-        assertEquals("search result [firstGen] exists", firstGen.getGroupId(), "sale");
-        assertEquals("search result [firstGen] children", firstGen.getChildren().size(), 0);
-        assertTrue("search result id exists", response.getResultId() != null);
+        assertEquals("browse result [root] exists", root.getGroupId(), "all");
+        assertEquals("browse result [firstGen] exists", firstGen.getGroupId(), "sale");
+        assertEquals("browse result [firstGen] children", firstGen.getChildren().size(), 0);
+        assertTrue("browse result id exists", response.getResultId() != null);
     }
 }

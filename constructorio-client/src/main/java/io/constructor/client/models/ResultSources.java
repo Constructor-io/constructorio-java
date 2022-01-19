492d87d996b065660f9bf4670857671f02c404cb
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2022-01-19 11:55:35 -0500

    Implement embeddings_match

---
+++ constructorio-client/src/main/java/io/constructor/client/models/ResultSources.java
@@ -0,0 +1,30 @@
+package io.constructor.client.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Constructor.io ResultSources ... uses Gson/Reflection to load data in
+ */
+
+public class ResultSources {
+
+  @SerializedName("token_match")
+  private ResultSourcesData tokenMatch;
+
+  @SerializedName("embeddings_match")
+  private ResultSourcesData embeddingsMatch;
+
+  /**
+   * @return the tokenMatch
+   */
+  public ResultSourcesData getTokenMatch() {
+    return tokenMatch;
+  }
+  
+  /**
+   * @return the embeddingsMatch
+   */
+  public ResultSourcesData getEmbeddingsMatch() {
+    return embeddingsMatch;
+  }
+}

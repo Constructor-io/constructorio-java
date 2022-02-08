20ee37782aaf2d2878234c753ebe8ebab2bf64cc
Stanley Peng <speng1011@gmail.com>
2022-02-08 14:32:48 -0800

    Add request models.

---
+++ constructorio-client/src/main/java/io/constructor/client/QuizRequest.java
@@ -0,0 +1,97 @@
+package io.constructor.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Constructor.io Next Quiz and Finalize Quiz request
+ */
+public class QuizRequest {
+    private String id;
+    private String indexKey;
+    private String section;
+    private List<List<Integer>> a;
+    private String versionId;
+
+    /**
+     * Creates a Next Quiz/Finalize Quiz request
+     *
+     * @param id the id of the quiz to request
+     * @param indexKey the index key of the product catalog
+     */
+    public QuizRequest(String id, String indexKey) {
+        if (id == null)
+            throw new IllegalArgumentException("id is a required parameter of type String");
+        if (indexKey == null)
+            throw new IllegalArgumentException("indexKey is a required parameter of type String");
+
+        this.section = null;
+        this.a = new ArrayList<>();
+        this.versionId = null;
+    }
+
+    /**
+     * @param id the id of the quiz to request
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the id of the quiz to request
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param indexKey the index_key/api_key of the product catalog
+     */
+    public void setIndexKey(String indexKey) {
+        this.indexKey = indexKey;
+    }
+
+    /**
+     * @return the index_key/api_key of the product catalog
+     */
+    public String getIndexKey() {
+        return indexKey;
+    }
+
+    /**
+     * @param section the section of the product catalog
+     */
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    /**
+     * @return the section of the product  catalog
+     */
+    public String getSection() {
+        return section;
+    }
+
+    /**
+     * @param a the list of answers in the format [ [1,2], [3,4] ] where [1,2] are the selected answers
+     *          for the first question and [3,4] are the selected answers for the second question
+     */
+    public void setA(List<List<Integer>> a) {
+        this.a = a;
+    }
+
+    /**
+     * @return the list of answers for the quiz
+     */
+    public List<List<Integer>> getA() { return a; }
+
+    /**
+     * @param versionId the specific version id for the quiz
+     */
+    public void setVersionId(String versionId) { this.versionId = versionId;}
+
+    /**
+     * @return the specific version id for the quiz
+     */
+    public String getVersionId() { return versionId; }
+}

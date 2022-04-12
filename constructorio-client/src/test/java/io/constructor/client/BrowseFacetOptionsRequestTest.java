df31b3daf05b3ea5fc61468bbfa586ed7594933d
Stanley Peng <speng1011@gmail.com>
2022-04-12 12:29:43 -0700

    Add tests

---
+++ constructorio-client/src/test/java/io/constructor/client/BrowseFacetOptionsRequestTest.java
@@ -0,0 +1,33 @@
+package io.constructor.client;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class BrowseFacetOptionsRequestTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void newShouldReturnBrowseFacetOptionsRequestWithDefaults() throws Exception {
+        BrowseFacetOptionsRequest request = new BrowseFacetOptionsRequest("Color");
+        assertEquals(0, request.getFormatOptions().size());
+        assertEquals("Color", request.getFacetName());
+    }
+
+    @Test
+    public void settersShouldSet() throws Exception {
+        BrowseFacetOptionsRequest request = new BrowseFacetOptionsRequest("Color");
+        assertEquals(0, request.getFormatOptions().size());
+        assertEquals("Color", request.getFacetName());
+
+        request.setFacetName("Brand");
+        request.getFormatOptions().put("show_hidden_facets", "True");
+
+        assertEquals(1, request.getFormatOptions().size());
+        assertEquals("Brand", request.getFacetName());
+    }
+}

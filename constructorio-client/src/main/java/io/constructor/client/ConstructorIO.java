fd2bc688484f2f652c7ba7a4faa1003ff0579f54
Steve Blaurock <sblaurock@gmail.com>
2022-06-03 12:53:43 -0600

    Version Bump

--- constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
+++ constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
@@ -1332,7 +1332,7 @@ public class ConstructorIO {
      * @return version number
      */
     protected String getVersion() {
-      return "ciojava-5.16.0";
+      return "ciojava-5.17.0";
     }
 
     /**

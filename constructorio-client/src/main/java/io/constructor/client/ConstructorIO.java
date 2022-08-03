ad4535f571fd0661fa5ce25470dd6144fabee0cd
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2022-08-03 11:48:28 -0400

    Add force and notificationEmail to items functions

--- constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
+++ constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
@@ -212,10 +212,11 @@ public class ConstructorIO {
      * @param items the items you want to add.
      * @param section the section of the autocomplete that you're adding the items to.
      * @param force whether or not the system should process the request even if it will invalidate a large number of existing variations.
+     * @param notificationEmail An email address where you'd like to receive an email notification in case the task fails.
      * @return true if working
      * @throws ConstructorException if the request is invalid.
      */
-    public boolean addOrUpdateItems(ConstructorItem[] items, String section, Boolean force) throws ConstructorException {
+    public boolean addOrUpdateItems(ConstructorItem[] items, String section, Boolean force, String notificationEmail) throws ConstructorException {
         try {
             HttpUrl url = this.makeUrl(Arrays.asList("v2", "items"));
             url = url
@@ -223,6 +224,14 @@ public class ConstructorIO {
                 .addQueryParameter("force", force.toString())
                 .addQueryParameter("section", section)
                 .build();
+
+            if (notificationEmail != null) {
+              url = url
+                .newBuilder()
+                .addQueryParameter("notification_email", notificationEmail)
+                .build();
+            }
+
             Map<String, Object> data = new HashMap<String, Object>();
             List<Object> itemsAsJSON = new ArrayList<Object>();
             for (ConstructorItem item : items) {
@@ -245,11 +254,15 @@ public class ConstructorIO {
     }
 
     public boolean addOrUpdateItems(ConstructorItem[] items) throws ConstructorException {
-        return addOrUpdateItems(items, "Products", false);
+        return addOrUpdateItems(items, "Products", false, null);
     }
 
     public boolean addOrUpdateItems(ConstructorItem[] items, String section) throws ConstructorException {
-        return addOrUpdateItems(items, section, false);
+        return addOrUpdateItems(items, section, false, null);
+    }
+
+    public boolean addOrUpdateItems(ConstructorItem[] items, String section, Boolean force) throws ConstructorException {
+        return addOrUpdateItems(items, section, force, null);
     }
 
     /**
@@ -257,16 +270,26 @@ public class ConstructorIO {
      *
      * @param items the items that you are removing
      * @param section the section of the autocomplete that you're removing the items from.
+     * @param force whether or not the system should process the request even if it will invalidate a large number of existing variations.
+     * @param notificationEmail An email address where you'd like to receive an email notification in case the task fails.
      * @return true if successfully removed
      * @throws ConstructorException if the request is invalid
      */
-    public boolean removeItems(ConstructorItem[] items, String section) throws ConstructorException {
+    public boolean removeItems(ConstructorItem[] items, String section, Boolean force, String notificationEmail) throws ConstructorException {
         try {
             HttpUrl url = this.makeUrl(Arrays.asList("v2", "items"));
             url = url
                 .newBuilder()
+                .addQueryParameter("force", force.toString())
                 .addQueryParameter("section", section)
                 .build();
+
+            if (notificationEmail != null) {
+              url = url
+                .newBuilder()
+                .addQueryParameter("notification_email", notificationEmail)
+                .build();
+            }
 
             Map<String, Object> data = new HashMap<String, Object>();
             List<Object> itemIds = new ArrayList<Object>();
@@ -291,6 +314,18 @@ public class ConstructorIO {
         } catch (Exception exception) {
             throw new ConstructorException(exception);
         }
+    }
+
+    public boolean removeItems(ConstructorItem[] items) throws ConstructorException {
+      return removeItems(items, "Products", false, null);
+    }
+
+    public boolean removeItems(ConstructorItem[] items, String section) throws ConstructorException {
+      return removeItems(items, section, false, null);
+    }
+
+    public boolean removeItems(ConstructorItem[] items, String section, Boolean force) throws ConstructorException {
+      return removeItems(items, section, force, null);
     }
 
     /**
@@ -408,10 +443,11 @@ public class ConstructorIO {
      * @param items the items that you're modifying.
      * @param section the section of the autocomplete that you're modifying the item for.
      * @param force whether or not the system should process the request even if it will invalidate a large number of existing variations.
+     * @param notificationEmail An email address where you'd like to receive an email notification in case the task fails.
      * @return true if successfully modified
      * @throws ConstructorException if the request is invalid.
      */
-    public boolean modifyItems(ConstructorItem[] items, String section, Boolean force) throws ConstructorException {
+    public boolean modifyItems(ConstructorItem[] items, String section, Boolean force, String notificationEmail) throws ConstructorException {
         try {
             HttpUrl url = this.makeUrl(Arrays.asList("v2", "items"));
             url = url
@@ -419,6 +455,14 @@ public class ConstructorIO {
                 .addQueryParameter("force", force.toString())
                 .addQueryParameter("section", section)
                 .build();
+
+            if (notificationEmail != null) {
+              url = url
+                .newBuilder()
+                .addQueryParameter("notification_email", notificationEmail)
+                .build();
+            }
+
             Map<String, Object> data = new HashMap<String, Object>();
             List<Object> itemsAsJSON = new ArrayList<Object>();
             for (ConstructorItem item : items) {
@@ -441,11 +485,15 @@ public class ConstructorIO {
     }
 
     public boolean modifyItems(ConstructorItem[] items) throws ConstructorException {
-        return modifyItems(items, "Products", false);
+        return modifyItems(items, "Products", false, null);
     }
 
     public boolean modifyItems(ConstructorItem[] items, String section) throws ConstructorException {
-        return modifyItems(items, section, false);
+        return modifyItems(items, section, false, null);
+    }
+
+    public boolean modifyItems(ConstructorItem[] items, String section, Boolean force) throws ConstructorException {
+        return modifyItems(items, section, force, null);
     }
 
     /**

ac97527036f47eb231b8d983c4fe8ff7253826a7
Jimmy Li <jimmyli921@gmail.com>
2022-09-22 16:08:48 -0700

    Refactor to handle arbitrary JSON data

--- constructorio-client/src/main/java/io/constructor/client/models/RefinedContent.java
+++ constructorio-client/src/main/java/io/constructor/client/models/RefinedContent.java
@@ -1,16 +1,17 @@
 package io.constructor.client.models;
 
 import com.google.gson.annotations.SerializedName;
+import java.util.Map;
 
 public class RefinedContent {
 
   @SerializedName("data")
-  private RefinedContentData data;
+  private Map<String, String> data;
 
   /**
    * @return refined content data
    */
-  public RefinedContentData getData() {
+  public Map<String, String> getData() {
     return data;
   }
 }

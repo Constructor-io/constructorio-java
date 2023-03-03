58c47c9e4997d24038b8639b6ef2058cd0c11348
ST P <57369888+stanlp1@users.noreply.github.com>
2023-03-03 11:26:45 -0800

    [CSL-2107] Add get all redirect rules support (#113)
    * Add get all redirect rules support
    
    * Format

---
+++ constructorio-client/src/main/java/io/constructor/client/models/AllRedirectsResponse.java
@@ -0,0 +1,39 @@
+package io.constructor.client.models;
+
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Constructor.io All Redirects Response ... uses Gson/Reflection to load data in
+ */
+public class AllRedirectsResponse {
+
+    @SerializedName("redirect_rules")
+    private List<RedirectRule> redirectRules;
+
+    @SerializedName("total_count")
+    private int totalCount;
+
+    /**
+     * @return the redirect_rules
+     */
+    public List<RedirectRule> getRedirectRules() {
+        return redirectRules;
+    }
+
+    /**
+     * @return the total_count
+     */
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setRedirectRules(List<RedirectRule> redirectRules) {
+        this.redirectRules = redirectRules;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+}
---
+++ constructorio-client/src/test/java/io/constructor/client/AllRedirectsRequestTest.java
@@ -0,0 +1,43 @@
+package io.constructor.client;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
+public class AllRedirectsRequestTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void newShouldReturnTaskRequestWithDefaults() throws Exception {
+        AllRedirectsRequest request = new AllRedirectsRequest();
+        assertEquals(0, request.getResultsPerPage());
+        assertEquals( 0, request.getPage());
+        assertEquals(0, request.getOffset());
+    }
+
+    @Test
+    public void settersShouldSet() throws Exception {
+        AllRedirectsRequest request = new AllRedirectsRequest();
+        assertEquals(0, request.getResultsPerPage());
+        assertEquals( 0, request.getPage());
+        assertEquals(0, request.getOffset());
+        assertEquals(null, request.getQuery());
+        assertEquals(null, request.getStatus());
+
+        request.setPage(5);
+        request.setResultsPerPage(50);
+        request.setOffset(3);
+        request.setQuery("bubble gum");
+        request.setStatus("expired");
+
+        assertEquals(50, request.getResultsPerPage());
+        assertEquals(5, request.getPage());
+        assertEquals(3, request.getOffset());
+        assertEquals("bubble gum", request.getQuery());
+        assertEquals("expired", request.getStatus());
+    }
+}

d24d1c40cbfb587b33ceb5f8891aeba8f4a1d15d
Jimmy Li <jimmyli921@gmail.com>
2023-09-06 09:36:50 -0700

    Version bump

--- constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
+++ constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
@@ -1924,7 +1924,7 @@ public class ConstructorIO {
      * @return version number
      */
     protected String getVersion() {
-        return "ciojava-5.24.0";
+        return "ciojava-5.24.1";
     }
 
     /**

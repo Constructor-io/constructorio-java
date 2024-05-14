342dd3eb6b5bee223f67cb1974de37cba831c173
mocca102 <islaam.agamy@gmail.com>
2024-05-14 22:51:59 +0300

    lint

--- constructorio-client/src/main/java/io/constructor/client/models/FilterFacetOption.java
+++ constructorio-client/src/main/java/io/constructor/client/models/FilterFacetOption.java
@@ -111,7 +111,7 @@ public class FilterFacetOption {
             }
         }
         throw new IllegalArgumentException(
-          "Each element of a range must be a Number or the string 'inf' or '-inf'"
+              "Each element of a range must be a Number or the string 'inf' or '-inf'"
         );
     }
 }

d3471853e4321a8ec3e29591cca1e72c1775ccdf
Stanley Peng <speng1011@gmail.com>
2024-05-17 13:52:23 -0700

    Version Bump

--- constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
+++ constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
@@ -1960,7 +1960,7 @@ public class ConstructorIO {
      * @return version number
      */
     protected String getVersion() {
-        return "ciojava-5.24.8";
+        return "ciojava-5.24.9";
     }
 
     /**

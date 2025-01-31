17833f8b8c6bedd8e1918faef4befb46bede9080
Islam Moustafa <58053149+mocca102@users.noreply.github.com>
2025-01-31 09:57:39 +0200

    [CI-3701] Add Support for deleting facet configurations (#153)
    * Delete Facet Configuration
    
    * lint

--- constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
+++ constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
@@ -2893,4 +2893,49 @@ public class ConstructorIO {
             throw new ConstructorException(exception);
         }
     }
+
+    /**
+     * Delete a facet configuration
+     *
+     * @param facetName the facet name
+     * @param section the section to which the facet belongs
+     * @return returns the deleted facet
+     * @throws ConstructorException if the request is invalid.
+     */
+    public String deleteFacetConfiguration(String facetName, String section)
+            throws ConstructorException {
+        try {
+            HttpUrl url =
+                    this.makeUrl(Arrays.asList("v1", "facets", facetName))
+                            .newBuilder()
+                            .addQueryParameter("section", section)
+                            .build();
+
+            Request request = this.makeAuthorizedRequestBuilder().url(url).delete().build();
+
+            Response response = client.newCall(request).execute();
+
+            return getResponseBody(response);
+        } catch (Exception exception) {
+            throw new ConstructorException(exception);
+        }
+    }
+
+    public String deleteFacetConfiguration(String facetName) throws ConstructorException {
+        return deleteFacetConfiguration(facetName, "Products");
+    }
+
+    /**
+     * Delete a facet configuration
+     *
+     * @param facetConfigurationRequest the facetConfiguration request
+     * @return returns the deleted facet
+     * @throws ConstructorException if the request is invalid.
+     */
+    public String deleteFacetConfiguration(FacetConfigurationRequest facetConfigurationRequest)
+            throws ConstructorException {
+        return deleteFacetConfiguration(
+                facetConfigurationRequest.geFacetConfiguration().getName(),
+                facetConfigurationRequest.getSection());
+    }
 }

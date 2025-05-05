0ad7777b21f5000e8f3969bdedae0d170342a7d5
Chris Gee <christopherrgee@gmail.com>
2025-05-05 09:06:12 -0700

    Version Bump

--- constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
+++ constructorio-client/src/main/java/io/constructor/client/ConstructorIO.java
@@ -2029,7 +2029,7 @@ public class ConstructorIO {
      * @return version number
      */
     protected String getVersion() {
-        return "ciojava-6.3.4";
+        return "ciojava-6.4.0";
     }
 
     /**

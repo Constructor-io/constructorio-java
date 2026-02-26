b2d008c00cfed21acddc1ab23138dd49a9831714
Andrei Andrukhovich <andrei.andrukhovich@constructor.io>
2026-02-26 15:06:56 -0700

    remove

--- constructorio-client/src/test/java/io/constructor/client/SearchabilityV2Test.java
+++ constructorio-client/src/test/java/io/constructor/client/SearchabilityV2Test.java
@@ -222,11 +222,6 @@ public class SearchabilityV2Test {
         new SearchabilitiesV2GetRequest(null);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testSearchabilitiesV2GetRequestWithEmptySectionThrowsException() {
-        new SearchabilitiesV2GetRequest("   ");
-    }
-
     @Test
     public void testSearchabilitiesV2DeleteRequest() {
         SearchabilitiesV2DeleteRequest request =
